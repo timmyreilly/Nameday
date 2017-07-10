89fda193bd63286f58cbe86e28331a4b4d04db4f
Tim Reilly <timreilly@live.com>
2017-07-10 14:42:11 -0700

    scrolls to current nameday

--- Nameday/AutoScrollToSelectedItemBehavior.cs
+++ Nameday/AutoScrollToSelectedItemBehavior.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using Microsoft.Xaml.Interactivity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Nameday
 {
-    class AutoScrollToSelectedItemBehavior
+    [TypeConstraint(typeof(ListViewBase))]
+    class AutoScrollToSelectedItemBehavior : DependencyObject, IBehavior
     {
+        private ListViewBase AssociatedListView => AssociatedObject as ListView; 
+
+
+        public DependencyObject AssociatedObject { get; private set; }
+
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            AssociatedObject = associatedObject;
+
+            if (AssociatedListView != null)
+                AssociatedListView.SelectionChanged += ListViewOnSelectionChanged; 
+        }
+
+        private void ListViewOnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AssociatedListView.SelectedItem == null)
+            {
+                return; 
+            }
+
+            AssociatedListView.ScrollIntoView(AssociatedListView.SelectedItem); 
+        }
+
+        public void Detach()
+        {
+            if (AssociatedListView != null)
+                AssociatedListView.SelectionChanged -= ListViewOnSelectionChanged; 
+        }
+
+
     }
 }
